060c9d08c050a38d7cc48e383a69ed87f830ccb6
bbgard <bgardiner@students.federation.edu.au>
2024-04-17 10:52:51 +1000

    Completed chapter 15

---
+++ auth.config.ts
@@ -0,0 +1,23 @@
+import type { NextAuthConfig } from 'next-auth';
+
+export const authConfig = {
+  pages: {
+    signIn: '/login',
+  },
+  callbacks: {
+    authorized({ auth, request: { nextUrl } }) {
+      const isLoggedIn = !!auth?.user; // !! converts value to a boolean
+      const isOnDashboard = nextUrl.pathname.startsWith('/dashboard');
+
+      // If user is on the dashboard, redirect to login page if not logged in, else redirect to dashboard
+      if (isOnDashboard) {
+        if (isLoggedIn) return true;
+        return false; // Redirect unauthenticated users to login page
+      } else if (isLoggedIn) {
+        return Response.redirect(new URL('/dashboard', nextUrl));
+      }
+      return true;
+    },
+  },
+  providers: [], // Add providers with an empty array for now
+} satisfies NextAuthConfig;
